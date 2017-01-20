8e9ac85573916f6949e8a84c0d53e331574e0377
Alastair Campbell <acampbell@nomensa.com>
2017-01-20 09:51:56 +0000

    Added a VERY basic font-family override script

---
+++ scripts/font-family.js
@@ -0,0 +1,36 @@
+// Initial bookmarklet code from https://www.smashingmagazine.com/2010/05/make-your-own-bookmarklets-with-jquery/ 
+
+
+(function(){
+
+	// the minimum version of jQuery we want
+	var v = "1.3.2";
+
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "//ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			
+			// Neutralise layout CSS
+
+			// Generally applicable
+			$("*").css('font-family', '"Comic Sans MS"');
+
+		})();
+	}
+
+})();
